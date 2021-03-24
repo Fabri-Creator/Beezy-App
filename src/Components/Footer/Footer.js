9dceaa8f1268dce4d8437e2dd38814c4aa16f31b
Fabri-Lan <fabriciolancieri@gmail.com>
2021-03-24 11:52:51 +0100

    Footer component development

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import "./Footer.scss";
+
+const Footer = () => {
+  return (
+    <div className="main-footer">
+      <ul className="ul-footer">
+        <li>ABOUT MARVEL</li>
+        <li>HELP/FAQS</li>
+        <li>CAREERS</li>
+        <li>INTERNSHIPS</li>
+      </ul>
+      <img src="../../../../marvel+beezy-logo.png" alt="marvel-logo" />
+    </div>
+  );
+};
+export default Footer;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,5 +1,5 @@
 import Header from "./../Components/Header";
-// import Footer from "./../Components/Footer";
+import Footer from "./../Components/Footer";
 
 import "./Pages.scss";
 
@@ -7,7 +7,7 @@ const Home = () => {
   return (
     <div className="main">
       <Header />
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 };
